3bc0292ca258c6364d705ddd669e135250f62d9e
김재현 <dacapo629@naver.com>
2026-02-25 10:28:06 +0900

    [level 1] Title: 약수의 개수와 덧셈, Time: 3.47 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
+++ 프로그래머스/1/77884. 약수의 개수와 덧셈/약수의 개수와 덧셈.java
@@ -5,10 +5,8 @@ class Solution {
         // 내부 루프에서 1부터 자기자신까지 반복
         // 약수의 갯수를 구함.
         // answer에 갯수가 짝수/홀수에 따라서 더하고 빼고 다음 루프 진행
-        
-        int count;
         for(int i=left; i<=right; i++){
-            count = 0;
+            int count = 0;
             for(int k=1; k<=i; k++){
                 if(i % k == 0){
                     count ++;
