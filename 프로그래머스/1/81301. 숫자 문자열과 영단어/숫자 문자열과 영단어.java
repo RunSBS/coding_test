f808bd27bd298ad0b21e6580fc27452e1d9e87df
김재현 <dacapo629@naver.com>
2026-04-13 11:56:33 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.34 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -1,9 +1,9 @@
 import java.util.*;
 class Solution {
     public int solution(String s) {
-        String answer = "";
+        int answer = 0;
         Map<String,String> map = new LinkedHashMap<>();
-        // Map에 영어 - 숫자 세팅
+        // Map에 <영어,숫자> 세팅
         map.put("zero","0");
         map.put("one","1");
         map.put("two","2");
@@ -15,14 +15,11 @@ class Solution {
         map.put("eight","8");
         map.put("nine","9");
         
-        // s에서, indexOf(map)을 활용해서 문자열 s의 영어를 숫자로 대체
+        // 문자열 s의 영어를 숫자로 대체
         for (String key : map.keySet()) {
-            if(s.indexOf(key) != -1){
-                s = s.replace(key, map.get(key));
-            }
+            s = s.replace(key, map.get(key));
         }
-        answer = s;
-        
-        return Integer.parseInt(answer);
+        answer = Integer.parseInt(s);
+        return answer;
     }
 }
